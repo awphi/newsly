161a42f262d2325364e11ae93541ba5f0ea62dba
awphi <jellymoos3@gmail.com>
2020-04-06 22:03:41 +0100

    use mock-fs

--- app/__tests__/app.test.js
+++ app/__tests__/app.test.js
@@ -2,36 +2,62 @@
 const app = require('../app');
 const request = require('supertest');
 const stories = require('../story-manager');
-const fs = require('fs');
-
-jest.mock('fs');
-
-beforeAll(() => {
-  fs.__setMockFiles({
-    data: {
-      test_story_1: {
-        'post.json':
-          '{"title": "Test Story 1 CATDOG", "views": 13, "subtitle": "Irrelevant subtitle here.","author": "John Doe","date": 1583430030752,"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","images": ["image1", "image2"]}',
-        'comments.json': '[{"author": "Julio Marucci","date": 1583430279222,"content": "Refried beans!"}]',
-        'image1.png': 'IMAGE BINARY',
-        'image2.png': 'IMAGE BINARY'
-      },
-      test_story_2: {
-        'post.json':
-          '{"title": "Test Story 2", "views": 2, "subtitle": "Irrelevant subtitle here.","author": "John Doe","date": 1580430030752,"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","images": ["image"]}',
-        'comments.json': '[{"author": "Julio Marucci","date": 1483430279222,"content": "Refried beans!"}]',
-        'image.png': 'IMAGE BINARY'
-      },
-      test_story_3: {
-        'post.json':
-          '{"title": "Test Story 3", "views": 14, "subtitle": "Irrelevant subtitle here.","author": "John Doe","date": 1283430030752,"body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","images": ["header"]}',
-        'comments.json': '[{"author": "Julio Marucci","date": 1383430279222,"content": "Refried beans!"}]',
-        'header.png': 'IMAGE BINARY'
-      }
+const mock = require('mock-fs');
+const testData = {
+  data: {
+    test_story_1: {
+      'post.json': JSON.stringify({
+        title: 'Test Story 1 CATDOG',
+        views: 13,
+        subtitle: 'Irrelevant subtitle here.',
+        author: 'John Doe',
+        date: 1583430030752,
+        body:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        images: ['image1', 'image2']
+      }),
+      'comments.json': JSON.stringify([{ author: 'Julio Marucci', date: 1583430279222, content: 'Refried beans!' }]),
+      'image1.png': 'IMAGE BINARY',
+      'image2.png': 'IMAGE BINARY'
+    },
+    test_story_2: {
+      'post.json': JSON.stringify({
+        title: 'Test Story 2',
+        views: 2,
+        subtitle: 'Irrelevant subtitle here.',
+        author: 'John Doe',
+        date: 1580430030752,
+        body:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        images: ['image']
+      }),
+      'comments.json': JSON.stringify([{ author: 'Julio Marucci', date: 1483430279222, content: 'Refried beans!' }]),
+      'image.png': 'IMAGE BINARY'
+    },
+    test_story_3: {
+      'post.json': JSON.stringify({
+        title: 'Test Story 3',
+        views: 14,
+        subtitle: 'Irrelevant subtitle here.',
+        author: 'John Doe',
+        date: 1283430030752,
+        body:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        images: ['header']
+      }),
+      'comments.json': JSON.stringify([{ author: 'Julio Marucci', date: 1383430279222, content: 'Refried beans!' }]),
+      'header.png': 'IMAGE BINARY'
     }
-  });
+  }
+};
 
-  return stories.load();
+beforeEach(() => {
+  mock(testData);
+  stories.load();
+});
+
+afterEach(() => {
+  mock.restore();
 });
 
 describe('GET endpoints', () => {
